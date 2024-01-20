759235f95940b89b1ffd711f6a17a1f68fb45f42
Inscolosus <Inscolosus@gmail.com>
2024-01-20 12:47:49 -0600

    max combo curve is now based on notes seen instead of notes in the map

--- HarmonyPatches/Score/ScoreController.cs
+++ HarmonyPatches/Score/ScoreController.cs
@@ -34,14 +34,14 @@ namespace BeatSaber5.HarmonyPatches.Score {
             int maxCombo = EnergyController.EnergyCounter.MaxCombo;
 
             double missCountCurve = noteCount / (50 * Math.Pow(misses, 2) + noteCount) * ((50d * noteCount + 1) / (50d * noteCount)) - 1 / (50d * noteCount);
-            double maxComboCurve = Math.Pow(noteCount / ((1 - Math.Sqrt(0.5)) * maxCombo - noteCount), 2) - 1; // TODO: something is weird with this and it messes up the score and causes the max score calculation to be wrong
+            double maxComboCurve = Math.Pow(TotalNotes / ((1 - Math.Sqrt(0.5)) * maxCombo - TotalNotes), 2) - 1; 
             //const double j = 1d / 1020734678369717893d;
             double accCurve = (19.0444 * Math.Tan((Math.PI / 133d) * acc - 4.22) + 35.5) * 0.01; // rip j
 
             int score = TotalCutScore == 0 || TotalNotes == 0 ? 0 : (int)(1_000_000d * ((missCountCurve * 0.3) + (maxComboCurve * 0.3) + (accCurve * 0.4)) * ((double)TotalNotes / (double)noteCount));
 
             ____multipliedScore = score;
-            ____immediateMaxPossibleMultipliedScore = (int)(1_000_000d * ((double)TotalNotes / (double)noteCount)); // temp fix while the max combo is weird
+            ____immediateMaxPossibleMultipliedScore = (int)(1_000_000d * ((double)TotalNotes / (double)noteCount)); 
 
             float totalMultiplier = ____gameplayModifiersModel.GetTotalMultiplier(____gameplayModifierParams, ____gameEnergyCounter.energy);
             ____modifiedScore = ScoreModel.GetModifiedScoreForGameplayModifiersScoreMultiplier(____multipliedScore, totalMultiplier);
