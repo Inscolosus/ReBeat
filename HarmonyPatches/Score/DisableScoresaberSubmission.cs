8a1bd88af3f488ad2e239cf77f94f46a8f455bdd
Inscolosus <Inscolosus@gmail.com>
2024-09-01 23:14:53 -0500

    remove unnecessary dependencies

--- HarmonyPatches/Score/DisableScoresaberSubmission.cs
+++ HarmonyPatches/Score/DisableScoresaberSubmission.cs
@@ -1,9 +1,15 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Linq;
+using System.Reflection;
 using HarmonyLib;
 
 namespace ReBeat.HarmonyPatches.Score {
     [HarmonyPatch]
     class DisableScoresaberSubmission {
+        static bool Prepare() {
+            return AppDomain.CurrentDomain.GetAssemblies().Any(asm => asm.GetName().Name == "ScoreSaber");
+        }
+        
         static MethodBase TargetMethod() {
             return AccessTools.TypeByName("ScoreSaber.Core.Daemons.UploadDaemon").GetMethod("Five");
         }
